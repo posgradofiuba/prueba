3cc2434e95a6e7d33a284617572c403d9e32fe61
Tom <tomyadrover@gmail.com>
2024-03-21 19:42:06 -0300

    Update search.js

--- search.js
+++ search.js
@@ -2,24 +2,30 @@ function search() {
     var searchTerm = document.getElementById("searchInput").value.trim().toLowerCase();
     if (!searchTerm) return;
 
-    var links = document.querySelectorAll("section a");
+    var sections = document.querySelectorAll("section");
     var searchResults = [];
 
-    links.forEach(function(link) {
-        var linkText = link.innerText.trim().toLowerCase();
-        var href = link.getAttribute("href");
+    sections.forEach(function(section) {
+        var h3Text = section.querySelector("h3").innerText.trim();
+        var links = section.querySelectorAll("a");
 
-        if (linkText.includes(searchTerm) && linkText.indexOf(searchTerm) === 0) {
-            var resultLink = document.createElement("a");
-            resultLink.href = href;
-            resultLink.textContent = linkText;
-            resultLink.target = "_blank";
-            
-            var resultItem = document.createElement("p");
-            resultItem.appendChild(resultLink);
-            
-            searchResults.push(resultItem);
-        }
+        links.forEach(function(link) {
+            var linkText = link.innerText.trim().toLowerCase();
+            var href = link.getAttribute("href");
+
+            if (linkText.includes(searchTerm) && linkText.indexOf(searchTerm) === 0) {
+                var resultLink = document.createElement("a");
+                resultLink.href = href;
+                resultLink.textContent = linkText;
+                resultLink.target = "_blank";
+
+                var resultItem = document.createElement("p");
+                resultItem.innerHTML = "<strong>" + h3Text + ":</strong> ";
+                resultItem.appendChild(resultLink);
+
+                searchResults.push(resultItem);
+            }
+        });
     });
 
     var searchResultsElement = document.getElementById("searchResults");
