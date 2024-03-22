861ea2cbff372055acc3747e6c12fb49f3322b09
Tom <tomyadrover@gmail.com>
2024-03-22 09:42:38 -0300

    Update search.js

--- search.js
+++ search.js
@@ -6,27 +6,22 @@ function search() {
     var searchResults = [];
 
     sections.forEach(function(section) {
-        var h3 = section.querySelector("h3");
-        if (!h3) {
-            console.error("No se encontró ningún elemento h3 dentro de la sección actual.");
-            return;
-        }
-        var h3Text = h3.innerText.trim();
+        var h2Text = section.querySelector("h2").innerText.trim(); // Obtenemos el texto del h2 de la sección
 
         var links = section.querySelectorAll("a");
-
         links.forEach(function(link) {
             var linkText = link.innerText.trim().toLowerCase();
             var href = link.getAttribute("href");
+            var h3Text = link.closest("section").querySelector("h3").innerText.trim(); // Obtenemos el h3 específico del enlace
 
-            if (linkText === searchTerm) {
+            if (linkText === searchTerm) { // Comprobamos si el texto del enlace coincide exactamente con el término de búsqueda
                 var resultLink = document.createElement("a");
                 resultLink.href = href;
-                resultLink.textContent = searchTerm;
+                resultLink.textContent = linkText;
                 resultLink.target = "_blank";
 
                 var resultItem = document.createElement("p");
-                resultItem.innerHTML = "<strong>" + h3Text + ": </strong>";
+                resultItem.innerHTML = "<strong>" + h2Text + " - " + h3Text + ": </strong>"; // Usamos el h3 específico del enlace
                 resultItem.appendChild(resultLink);
 
                 searchResults.push(resultItem);
