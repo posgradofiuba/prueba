0093cb5e97e7a0efb32d23a4bcbc7f9ac489a1fb
Tom <tomyadrover@gmail.com>
2024-03-22 09:57:55 -0300

    Update search.js

--- search.js
+++ search.js
@@ -7,22 +7,21 @@ function search() {
 
     sections.forEach(function(section) {
         var h2Text = section.querySelector("h2").innerText.trim(); // Obtenemos el texto del h2 de la sección
-        var h3Text = section.querySelector("h3").innerText.trim(); // Obtenemos el texto del h3 de la sección
 
         var links = section.querySelectorAll("a");
         links.forEach(function(link) {
             var linkText = link.innerText.trim().toLowerCase();
             var href = link.getAttribute("href");
+            var h3Text = link.closest("section").querySelector("h3").innerText.trim(); // Obtenemos el h3 específico del enlace
 
-            // Extraemos el número de resolución del texto del enlace
-            var resolutionNumber = linkText.match(/\d+\/\d+/);
-            if (resolutionNumber && resolutionNumber[0] === searchTerm) { // Comprobamos si el número de resolución coincide exactamente con el término de búsqueda
+            if (linkText === searchTerm) { // Comprobamos si el texto del enlace coincide exactamente con el término de búsqueda
                 var resultLink = document.createElement("a");
                 resultLink.href = href;
-                resultLink.textContent = h2Text + ' - ' + h3Text + ': ' + resolutionNumber[0]; // Mostramos el número de resolución en el enlace
+                resultLink.textContent = linkText;
                 resultLink.target = "_blank";
 
                 var resultItem = document.createElement("p");
+                resultItem.innerHTML = "<strong>" + h2Text + " - " + h3Text + ": </strong>"; // Usamos el h3 específico del enlace
                 resultItem.appendChild(resultLink);
 
                 searchResults.push(resultItem);
