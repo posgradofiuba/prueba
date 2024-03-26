b93c3fcfe8fcfc2f096ee146f35fca2c9cfdd83a
Tom <tomyadrover@gmail.com>
2024-03-26 09:42:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function scrollToTop() {
     document.body.scrollTop = 0; // Para Safari
     document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 }
+// Obtener todos los elementos con la clase "dropdown-toggle"
+const toggles = document.querySelectorAll('.dropdown-toggle');
+
+// Agregar un evento de clic a cada toggle
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', function() {
+    // Obtener el siguiente elemento hermano (la lista desplegable)
+    const content = this.nextElementSibling;
+
+    // Alternar la visibilidad de la lista desplegable
+    content.style.display === 'none' ? content.style.display = 'block' : content.style.display = 'none';
+  });
+});
