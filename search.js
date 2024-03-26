ad844aaf41057506a1a78addcd275f60d111c4f0
Tom <tomyadrover@gmail.com>
2024-03-26 09:33:46 -0300

    Update search.js

--- search.js
+++ search.js
@@ -1,36 +1,40 @@
 function search() {
     var searchTerm = document.getElementById("searchInput").value.trim().toLowerCase();
-    if (!searchTerm) return;
+    var searchResultsElement = document.getElementById("searchResults");
+
+    // Limpiar los resultados si el campo de búsqueda está vacío
+    if (!searchTerm) {
+        searchResultsElement.innerHTML = "";
+        return;
+    }
 
     var sections = document.querySelectorAll("section");
     var searchResults = [];
-
     sections.forEach(function(section) {
         var links = section.querySelectorAll("a");
         links.forEach(function(link) {
             var linkText = link.innerText.trim().toLowerCase();
             var href = link.getAttribute("href");
             var h3Text = link.closest("ul").previousElementSibling.innerText.trim();
-
-            if (linkText === searchTerm) {
+            
+            // Verificar si el texto del enlace incluye el término de búsqueda
+            if (linkText.includes(searchTerm)) {
                 var resultLink = document.createElement("a");
                 resultLink.href = href;
                 resultLink.textContent = linkText;
                 resultLink.target = "_blank";
-
                 var trimmedH3Text = h3Text.replace("Laboratorio de Sistemas Embebidos (LSE) - ", "");
                 var resultItem = document.createElement("p");
                 resultItem.innerHTML = "<strong>" + trimmedH3Text + ": </strong>";
                 resultItem.appendChild(resultLink);
-
                 searchResults.push(resultItem);
             }
         });
     });
 
-    var searchResultsElement = document.getElementById("searchResults");
-    searchResultsElement.innerHTML = "";
+    // Mostrar o limpiar los resultados
     if (searchResults.length > 0) {
+        searchResultsElement.innerHTML = "";
         searchResults.forEach(function(result) {
             searchResultsElement.appendChild(result);
         });
